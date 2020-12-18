d59168b9bc5656989820d9ebda54ca1066aa6f73
Balázs Kilvády <bkilvady@gmail.com>
2020-12-18 15:59:57 +0100

    Initial Commit

---
+++ Shared/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  Shared
+//
+//  Created by Balázs Kilvády on 2020. 12. 18..
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
